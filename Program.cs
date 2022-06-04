ee05cd32bacde3b76bf4d862379e8bdc837988f0
camilalocatelli <locatelli.camila6@gmail.com>
2022-06-03 21:57:25 -0300

    Consertado

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a, b, resto;
+
+Console.WriteLine("Máximo Divisor Comum (método iterativo)");
+
+Console.Write("Digite o primeiro número: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+do
+{
+    resto = a % b;
+    a = b;
+    b = resto;
+}
+
+while
+(resto != 0);
+
+Console.WriteLine($"MDC(a, b) = {a}");
